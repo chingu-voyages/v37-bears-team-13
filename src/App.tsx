1b3dd3b7c93788ce6529f858b8ce78d53e8fe26d
R2D2 <66644638+R2rit0@users.noreply.github.com>
2022-02-21 11:04:44 -0500

    refactor: organizing code

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import "./App.css";
 
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
-import StockListing from "./pages/StockListing/StockListing";
 
 function App() {
   return (
     <div className="App">
-      <StockListing />
+      <header>
+        <h1>Stock Race</h1>
+        <NavBar />
+        <Home />
+      </header>
     </div>
   );
 }
