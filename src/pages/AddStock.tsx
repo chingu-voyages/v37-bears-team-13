17f62901f643a397bb035b3ef58b4e97db45bc9b
Regina McGuire <rmcguire@as.net>
2022-03-11 18:41:30 -0800

    feat: Add stocks variable to AddStocks

--- src/pages/AddStock.tsx
+++ src/pages/AddStock.tsx
@@ -2,17 +2,23 @@ import { useState } from 'react';
 
 const AddFavoriteStock = (): JSX.Element => {
   const [stockSymbol, setStockSymbol] = useState('');
+  const [stocks, setStocks] = useState([]);
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const stockSymbol = e.target.value;
     setStockSymbol(stockSymbol);
   };
-
+  const addStockSymbol = (symbol: string) => {};
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const target = e.target as typeof e.target & {
       stockSymbol: { value: string };
     };
+    //@ts-ignore
+    setStocks((previousState) => {
+      return [...previousState, stockSymbol];
+    });
+    setStockSymbol('');
   };
   return (
     <div className="container">
@@ -31,6 +37,10 @@ const AddFavoriteStock = (): JSX.Element => {
         <br />
         <input type="submit" />
       </form>
+      {/* for debugging until the backend is hooked up 
+      {stocks.map((stock) => (
+        <p>{stock}</p>
+      ))} */}
     </div>
   );
 };
