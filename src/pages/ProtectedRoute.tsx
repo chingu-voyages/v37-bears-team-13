96824635de1d636bfc784faeedc77058587869e8
Regina McGuire <rmcguire@as.net>
2022-03-14 16:11:29 -0700

    bug: Add Props interface

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,10 +1,15 @@
 import { Navigate } from 'react-router-dom';
 
+export interface Props {
+  user: string;
+  redirectPath: string;
+  children: JSX.Element;
+}
 const ProtectedRoute = ({
   user,
   redirectPath = '/',
   children
-}): JSX.Element => {
+}: Props): JSX.Element => {
   if (!user) {
     return <Navigate to={redirectPath} replace />;
   }
