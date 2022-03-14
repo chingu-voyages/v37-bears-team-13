07d2d31b092f26f176d4bc977d3f96b90e32decf
Regina McGuire <rmcguire@as.net>
2022-03-14 15:05:47 -0700

    style: Style Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import NavBar from '../NavBar';
-import './styles.module.css';
+
+import NavBar from '../NavBar/NavBar';
+import './header.css';
+import racingCar from '../../images/racing-car.svg';
 const Header = (): JSX.Element => {
   return (
-    <div className="container">
-      <h1>Stock Race</h1>
+    <div className="header">
+      <div className="header-box">
+        <h1>Stock Race...</h1>
+        <img src={racingCar} alt="racing car" />
+      </div>
+
       <NavBar />
     </div>
   );
