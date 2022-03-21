aab2c9fa0a1a69613826a79f44018b35cef29dba
Regina McGuire <rmcguire@as.net>
2022-03-20 19:17:38 -0700

    refactor: Simplify Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
-import StockViewer from '../components/StockViewer';
 import Listing from '../pages/StockListing/StockListing';
 import { useAppCtx } from 'context';
-
+const titleStyle = {
+  fontSize: '1.4rem',
+  fontWeight: '700'
+};
 const Home = (): JSX.Element => {
   const { loggedIn } = useAppCtx();
-
   return (
-    <div>
-      <StockViewer
-        stockView={loggedIn ? 'User Stock View' : 'Example Stock View'}
-      />
+    <div className="container">
+      <div style={titleStyle}>
+        {loggedIn ? 'User Stock View' : 'Example Stock View'}
+      </div>
       <Listing />
     </div>
   );
